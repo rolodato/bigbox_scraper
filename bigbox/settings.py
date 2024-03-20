6413e9ea8ed0acf6baf5cfa55663cdb3e8ddf7ef
Rodrigo López Dato <rolodato@rolodato.com>
2024-03-20 09:29:19 -0300

    base

--- bigbox/settings.py
+++ bigbox/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "bigbox.spiders"
 #USER_AGENT = "bigbox (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
@@ -91,3 +91,8 @@ ROBOTSTXT_OBEY = True
 REQUEST_FINGERPRINTER_IMPLEMENTATION = "2.7"
 TWISTED_REACTOR = "twisted.internet.asyncioreactor.AsyncioSelectorReactor"
 FEED_EXPORT_ENCODING = "utf-8"
+
+DOWNLOAD_HANDLERS = {
+    "http": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+    "https": "scrapy_playwright.handler.ScrapyPlaywrightDownloadHandler",
+}
--- bigbox/spiders/experiences.py
+++ bigbox/spiders/experiences.py
@@ -4,7 +4,7 @@ import scrapy
 class ExperiencesSpider(scrapy.Spider):
     name = "experiences"
     allowed_domains = ["bigbox.com.ar"]
-    start_urls = ["https://bigbox.com.ar"]
+    start_urls = ["https://www.bigbox.com.ar/catalogo/eyJpZCI6NDIwNDI2N30:1rmeUV:TU4bJPjiJ6_NjzCW3Pt7odf4qL4/bonjour/"]
 
     def parse(self, response):
         pass
